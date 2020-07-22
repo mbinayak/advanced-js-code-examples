2327d218bfb19c7ffc90d1a4899e7f5fed07a062
binayak <mbinayak@ymail.com>
2020-07-22 07:30:21 +0530

    __proto__ examples added

--- src/inheritance-techniques.js
+++ src/inheritance-techniques.js
@@ -1,2 +1,22 @@
-// TODO
+/*
+  __proto__ experiments
+*/
 
+let obj1 = {
+    propA: "valueA1",
+    propB: "valueB1"
+};
+
+let obj2 = {
+    propA: "valueA2",
+    propC: "valueC2"
+};
+
+obj2.__proto__ = obj1;
+
+obj2.propB = "something-else";
+
+console.log(obj1, obj2); // { propA: 'valueA1', propB: 'valueB1' } { propA: 'valueA2', propC: 'valueC2', propB: 'something  else' }
+console.log(obj1.propB, obj2.propB); //  valueB1 something-else
+console.log(obj1.propA, obj2.propA); // valueA1 valueA2
+console.log(obj1.propC, obj2.propC); // undefined valueC2

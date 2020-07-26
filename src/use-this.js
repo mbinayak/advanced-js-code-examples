7fb532baf7e2f2273fc191dab15fcc3532a24e6f
Binayak Mishra <mbinayak@ymail.com>
2020-07-26 23:09:07 +0530

    this use exp

--- src/use-this.js
+++ src/use-this.js
@@ -1,1 +1,25 @@
-// TODO
+'use strict';
+
+const obj = {
+    a: 34,
+    name: 'Joe',
+    printNameArrow: () => {
+        console.log(this);
+        console.log(this.name);
+    },
+    printName: function () {
+        console.log(this);
+        console.log(this.name);
+    }
+}
+
+const f1 = obj.printNameArrow.bind(obj);
+const f2 = obj.printNameArrow;
+
+f1();
+f2();
+f2.call(obj, []);
+f2.apply(obj);
+// undefined
+obj.printName();
+// Joe

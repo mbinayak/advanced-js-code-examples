0397a212a281be9539b3dd08bd38514d288da481
Binayak Mishra <mbinayak@ymail.com>
2020-07-29 20:19:48 +0530

    simple  async await example

--- src/promises-async-await.js
+++ src/promises-async-await.js
@@ -1,1 +1,16 @@
 // TODO
+
+const apiCall = () => fetch('https://jsonplaceholder.typicode.com/todos/1');
+
+// promise api
+apiCall()
+    .then(response => response.json())
+    .then(json => console.log(json));
+
+asyncApiCall = async () => {
+    const res = await apiCall();
+
+    console.log(res);
+};
+
+

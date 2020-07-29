2dfb5a4c651dad79989ac47f7f0be5610a9e7c00
Binayak Mishra <mbinayak@ymail.com>
2020-07-29 20:21:05 +0530

    this more exp

--- src/use-this.js
+++ src/use-this.js
@@ -4,22 +4,24 @@ const obj = {
     a: 34,
     name: 'Joe',
     printNameArrow: () => {
+        console.log(arguments);
         console.log(this);
         console.log(this.name);
     },
     printName: function () {
+        console.log(arguments);
         console.log(this);
         console.log(this.name);
     }
 }
 
-const f1 = obj.printNameArrow.bind(obj);
+const f1 = obj.printNameArrow.bind(obj); // arrow funcitons only bind this to lexical order
 const f2 = obj.printNameArrow;
 
 f1();
 f2();
-f2.call(obj, []);
-f2.apply(obj);
+f2.call(obj, []); // arrow funcitons only bind this to lexical order
+f2.apply(obj); // arrow funcitons only bind this to lexical order
 // undefined
 obj.printName();
 // Joe

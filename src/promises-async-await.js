fe1faa125f21c3643e40e98e63c7915a5cc51717
Binayak Mishra <mbinayak@ymail.com>
2020-07-30 18:28:29 +0530

    more on async and await

--- src/promises-async-await.js
+++ src/promises-async-await.js
@@ -1,4 +1,4 @@
-// TODO
+'use strict';
 
 const apiCall = () => fetch('https://jsonplaceholder.typicode.com/todos/1');
 
@@ -12,5 +12,68 @@ asyncApiCall = async () => {
 
     console.log(res);
 };
+
+asyncApiCall();
+
+
+// creating an async task
+/*
+myAsyncTask: async task will call resolve fucnction(resFunc) on success
+*/
+const myAsyncTask = () => new Promise((resFunc, rejFunc) => {
+    try {
+        setTimeout(() => resFunc(), 1000);
+    } catch (e) {
+        rejFunc(e);
+    }
+});
+
+/*
+myAsyncTaskInverse: Inverse async task will call reject function(rejFunc) on success
+*/
+const myAsyncTaskInverse = () => new Promise((resFunc, rejFunc) => {
+    try {
+        setTimeout(() => rejFunc(), 1000);
+    } catch (e) {
+        console.log(e);
+        resFunc();
+    }
+});
+
+try {
+    myAsyncTask().then(() => {
+        console.log('Promise1 resolved!');
+    }).catch(e => {
+        console.log(`Error while fullfilling promise: ${e.message}`);
+    });
+} catch (e) {
+    console.log(`Promise1 error caUGHT! ${e.message}`);
+}
+
+try {
+    myAsyncTaskInverse().then(() => {
+        console.log('Promise2 resolved!');
+    });
+} catch (e) {
+    console.log(`Promise2 error caUGHT! ${e.message}`);
+}
+
+try {
+    myAsyncTask().then(() => {
+        console.log('Inverse Promise1 resolved!');
+    }).catch(() => {
+        console.log('Inverse Promise1 catch function- rejFunc executed');
+    });
+} catch (e) {
+    console.log(`Inverse Promise1 error caUGHT! ${e.message}`);
+}
+
+try {
+    myAsyncTask().then(() => {
+        console.log('Inverse Promise2 resolved!');
+    });
+} catch (e) {
+    console.log(`Inverse Promise2 error caUGHT! ${e.message}`);
+}
 
 
